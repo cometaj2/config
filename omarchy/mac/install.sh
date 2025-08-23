af53da33d4344fe1e4fde00aac9cf673942f2a40
Jeff Michaud <cometaj2@proton.me>
2025-08-23 09:53:54 -0700

    move omarchy to mac folder and add mbpfan configuration and installation

--- omarchy/mac/install.sh
+++ omarchy/mac/install.sh
@@ -8,16 +8,21 @@ mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Baseline terminal configuration
-cp ../.bash_profile ~
-cp ../.vimrc ~
-cp ../.gitconfig ~
+cp ../../.bash_profile ~
+cp ../../.vimrc ~
+cp ../../.gitconfig ~
 
 source ~/.bash_profile
 
-# Removal of chromium and wayland configuration, which causes
+# Removal of ozone-platform wayland flag, which causes glitches and replace chromium for brave.
 #HYPRLAND_BINDINGS="~/.config/hypr/bindings.conf
 
 yes | yay -R chromium
 yes | yay -S brave-bin
 yes | yay -S keeper-password-manager
 
+# mbp fan configuration to help manage mac fan speed
+yes | yay -S mbpfan
+sudo cp ./etc/mbpfan.conf /etc/mbpfan.conf
+sudo systemctl enable mbpfan
+sudo systemctl start mbpfan
